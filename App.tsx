6ce21091f6a07279fea9ad7b264d3cd3b4f3ad79
Casteluke Creative Labs - Devs <151608776+CastelukeDevs@users.noreply.github.com>
2024-01-22 07:02:31 +0700

    New: StatusBar settings

--- App.tsx
+++ App.tsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import Routes from './src/Routes/Routes';
 
-export default () => <Routes />;
+export default () => (
+  <>
+    <StatusBar
+      translucent
+      backgroundColor="transparent"
+      barStyle="dark-content"
+    />
+    <Routes />
+  </>
+);
