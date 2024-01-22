6d9819905985eb9d228710d84851bb6eea5369c4
Casteluke Creative Labs - Devs <151608776+CastelukeDevs@users.noreply.github.com>
2024-01-22 08:18:09 +0700

    New: Text Input

--- src/Components/Common/Icon.tsx
+++ src/Components/Common/Icon.tsx
@@ -1,37 +1,94 @@
-import React from 'react';
-import {TouchableOpacity} from 'react-native';
+import React, {forwardRef} from 'react';
+import {TextStyle} from 'react-native';
+import {ThemeColor} from '@Utilities/Styles/GlobalStyles';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 
-import {ThemeColor} from '@Utilities/Styles/GlobalStyles';
-import {IIconName} from '@Utilities/Tools/IconTools';
+enum IconMode {
+  outline,
+  filled,
+  sharp,
+}
+
+enum IconList {
+  'arrow-back',
+  'at',
+  'calendar',
+  'card',
+  'cash',
+  'checkmark',
+  'chevron-back',
+  'close',
+  'desktop',
+  'eye',
+  'eye-off',
+  'home',
+  'lock-closed',
+  'lock-open',
+  'logo-usd',
+  'mail',
+  'notifications',
+  'pencil',
+  'person',
+  'rocket',
+}
+
+export type IIconMode = keyof typeof IconMode;
+export type IIconName = keyof typeof IconList;
+type IIcon = IIconName;
 
 export type IIconProps = {
-  name?: IIconName;
-  color?: string;
+  name?: IIcon;
   size?: number;
-  onPress?: () => void;
+  color?: string;
+  mode?: IIconMode;
   disabled?: boolean;
+  onPress?: () => void;
+  style?: TextStyle;
 };
 
-const Icon = (props: IIconProps) => {
-  const iconName = props.name || 'home';
-  const iconColor = props.color || ThemeColor.dark;
-  const iconSize = props.size || 20;
-  const isDisabled = props.disabled || typeof props.onPress === 'undefined';
+const getIconName = (name: IIcon, mode?: IIconMode): IIcon => {
+  const selectedMode = mode || 'outline';
+  if (name.includes('logo')) return name;
+  if (name.includes('outline')) return name;
+  return (name +
+    (selectedMode === 'filled' ? '' : `-${selectedMode}`)) as IIcon;
+};
+
+/**
+ * Icon components extends RN Vector icons using IonIcons
+ *
+ * @param name icon name
+ * @param size icon size
+ * @param color icon color
+ * @param mode icon mode
+ *
+ * @default "home"
+ * @default 20
+ * @default ThemeDark
+ * @default "outline"
+ * @returns
+ */
+const Icon = forwardRef<IonIcon, IIconProps>((props, ref) => {
+  const {
+    name = 'home',
+    size = 20,
+    mode = 'outline',
+    color = ThemeColor.dark,
+  } = props; //default value
+
+  const iconName = getIconName(name, mode);
 
   return (
-    <TouchableOpacity
-      disabled={isDisabled}
-      style={{height: iconSize, width: iconSize}}
-      onPress={() => props.onPress?.()}>
-      <IonIcon
-        name={iconName}
-        color={iconColor}
-        size={iconSize}
-        // onPress={() => props.onPress?.()}
-      />
-    </TouchableOpacity>
+    <IonIcon
+      ref={ref}
+      name={iconName}
+      size={size}
+      color={color}
+      disabled={typeof props.onPress === 'undefined' || props.disabled}
+      onPress={props.onPress}
+      style={props.style}
+    />
   );
-};
+});
 
 export default Icon;
